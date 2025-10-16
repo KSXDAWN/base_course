43864c1822650566618baba57baf148db7c57ed8
KSXDAWN <denis12458971@gmail.com>
2025-10-16 14:17:40 +0000

    KARMAgit status!

---
+++ lec_data_type.py
@@ -0,0 +1,22 @@
+type
+
+type(3)
+
+print(type(3))
+
+print(type(3.4))
+
+print(type("ksdfl;kds"))
+
+a = 'kjlsdkfllsd'
+print(type(a))
+
+a = ['fosdfikl', 1, 'jkfklaf', 6, 8]
+print(type(a))
+
+print(type(True))
+
+print(type(False))
+
+print(type(None))
+print(print(3))
---
+++ task_1.py
@@ -0,0 +1,14 @@
+print('Hello World!')
+
+a = 'Маша'
+b = 'Петя'
+c = 'Любовь'
+print(a, '+', b, '=', c)
+
+d = 3
+f = 4
+g = (d, '+', f)
+print('x =', g)
+
+x = (d + f)
+print('x =', x)
